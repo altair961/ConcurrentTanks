ff0366a53312dc3f03f71c27d187b14a8d3a98b5
altair961 <altair89@list.ru>
2026-05-28 13:06:38 +0300

    Add "hello world"

--- src/ConcurrentTanks.Client/Program.cs
+++ src/ConcurrentTanks.Client/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConcurrentTanks.Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World 2!");
+        }
+    }
+}
