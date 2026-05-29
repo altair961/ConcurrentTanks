335ab95dc74f63156ed49c524ffaf57a3607ed33
altair961 <altair89@list.ru>
2026-05-29 22:33:33 +0300

    Implement hello window

--- src/ConcurrentTanks.Client/Program.cs
+++ src/ConcurrentTanks.Client/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
 
 namespace ConcurrentTanks.Client
 {
     public class Program
     {
+        private static IWindow _window;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World 2!");
+
+            WindowOptions options = WindowOptions.Default with
+            {
+                Size = new Vector2D<int>(800, 600),
+                Title = "My first Silk.NET application!"
+            };
+
+            _window = Window.Create(options);
+            _window.Load += OnLoad;
+            _window.Update += OnUpdate;
+            _window.Render += OnRender;
+            _window.Run();
         }
+
+        private static void OnLoad() { }
+
+        private static void OnUpdate(double deltaTime) { }
+
+        private static void OnRender(double deltaTime) { }
     }
 }
