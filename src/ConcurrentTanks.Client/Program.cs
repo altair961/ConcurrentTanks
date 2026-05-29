fa21790e5cbc155c78105f5ad29f2f12490210d1
altair961 <altair89@list.ru>
2026-05-29 23:09:15 +0300

    Implement clear color

--- src/ConcurrentTanks.Client/Program.cs
+++ src/ConcurrentTanks.Client/Program.cs
@@ -1,12 +1,15 @@
 ﻿using Silk.NET.Input;
 using Silk.NET.Maths;
 using Silk.NET.Windowing;
+using Silk.NET.OpenGL;
+using System.Drawing;
 
 namespace ConcurrentTanks.Client
 {
     public class Program
     {
         private static IWindow _window;
+        private static GL _gl;
 
         static void Main(string[] args)
         {
@@ -30,11 +33,16 @@ namespace ConcurrentTanks.Client
             IInputContext input = _window.CreateInput();
             for (int i = 0; i < input.Keyboards.Count; i++)
             input.Keyboards[i].KeyDown += KeyDown;
+            _gl = _window.CreateOpenGL();
+            _gl.ClearColor(Color.CornflowerBlue);
         }
 
         private static void OnUpdate(double deltaTime) { }
 
-        private static void OnRender(double deltaTime) { }
+        private static void OnRender(double deltaTime)
+        {
+            _gl.Clear(ClearBufferMask.ColorBufferBit);
+        }
 
         private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
         {
