9bef502ea866904fe5a945681247b67dba09a300
altair961 <altair89@list.ru>
2026-05-30 14:43:39 +0300

    Show rectangle

--- src/ConcurrentTanks.Client/Program.cs
+++ src/ConcurrentTanks.Client/Program.cs
@@ -13,6 +13,7 @@ namespace ConcurrentTanks.Client
         private static uint _vao;
         private static uint _vbo;
         private static uint _ebo;
+        private static uint _program;
 
         static void Main(string[] args)
         {
@@ -69,6 +70,70 @@ namespace ConcurrentTanks.Client
             fixed (uint* buf = indices)
                 _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint) (indices.Length * sizeof(uint)), 
                     buf, BufferUsageARB.StaticDraw);
+
+            const string vertexCode = @"
+#version 330 core
+
+layout (location = 0) in vec3 aPosition;
+
+void main()
+{
+    gl_Position = vec4(aPosition, 1.0);
+}";
+
+        const string fragmentCode = @"
+#version 330 core
+
+out vec4 out_color;
+
+void main()
+{
+    out_color = vec4(1.0, 0.5, 0.2, 1.0);
+}";
+
+            uint vertexShader = _gl.CreateShader(ShaderType.VertexShader);
+            _gl.ShaderSource(vertexShader, vertexCode);
+
+            _gl.CompileShader(vertexShader);
+
+            _gl.GetShader(vertexShader, ShaderParameterName.CompileStatus, out int vStatus);
+            if (vStatus != (int) GLEnum.True)
+                throw new Exception("Vertex shader failed to compile: " + _gl.GetShaderInfoLog(vertexShader));
+
+            uint fragmentShader = _gl.CreateShader(ShaderType.FragmentShader);
+            _gl.ShaderSource(fragmentShader, fragmentCode);
+
+            _gl.CompileShader(fragmentShader);
+
+            _gl.GetShader(fragmentShader, ShaderParameterName.CompileStatus, out int fStatus);
+            if (fStatus != (int) GLEnum.True)
+                throw new Exception("Fragment shader failed to compile: " + _gl.GetShaderInfoLog(fragmentShader));
+
+            _program = _gl.CreateProgram();
+
+            _gl.AttachShader(_program, vertexShader);
+            _gl.AttachShader(_program, fragmentShader);
+
+            _gl.LinkProgram(_program);
+
+            _gl.GetProgram(_program, ProgramPropertyARB.LinkStatus, out int lStatus);
+            if (lStatus != (int) GLEnum.True)
+                throw new Exception("Program failed to link: " + _gl.GetProgramInfoLog(_program));
+
+            _gl.DetachShader(_program, vertexShader);
+            _gl.DetachShader(_program, fragmentShader);
+            _gl.DeleteShader(vertexShader);
+            _gl.DeleteShader(fragmentShader);
+
+            const uint positionLoc = 0;
+            _gl.EnableVertexAttribArray(positionLoc);
+            _gl.VertexAttribPointer(positionLoc, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), (void*) 0);
+
+            _gl.BindVertexArray(0);
+            _gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+            _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, 0);
+
+            _gl.Viewport(_window.FramebufferSize);
         }
 
         private static void OnUpdate(double deltaTime) { }
@@ -76,6 +141,9 @@ namespace ConcurrentTanks.Client
         private static unsafe void OnRender(double deltaTime)
         {
             _gl.Clear(ClearBufferMask.ColorBufferBit);
+            _gl.BindVertexArray(_vao);
+            _gl.UseProgram(_program);
+            _gl.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, (void*) 0);
         }
 
         private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
